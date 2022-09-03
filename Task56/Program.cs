70ce5027e8ee9bb3705a3bd1ddc6887c7829d6dc
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-09-03 17:27:51 +0300

    Task56 done

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class program
+{
+
+    static int SmallestAmountLine(int[,] array)
+    {
+        int min = 0;
+        int[] Sum = new int[array.GetLength(0)];
+        int NumLine = 0;
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Sum[i] += array[i, j];
+            }
+            if (i == 0)
+            {
+                min = Sum[i];
+                NumLine = i + 1;
+            }
+            else if (Sum[i] < min)
+            {
+                min = Sum[i];
+                NumLine = i + 1;
+            }
+        }
+        return NumLine;
+    }
+
+    static void WriteArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(" " + array[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Main()
+    {
+        int[,] someArray = {{1, 4, 7, 2,},
+                            {5, 9, 2, 3},
+                            {8, 4, 2, 4},
+                            {5, 2, 6, 7}};
+
+        Console.Clear();
+        Console.WriteLine("Исходный массив: ");
+
+        WriteArray(someArray);
+        Console.WriteLine();
+        Console.WriteLine("Номер строки с наименьшей суммой элементов: " + SmallestAmountLine(array: someArray));
+
+    }
+}
